7ea9e3d9081a9337c16b367c8d97567d82cf8328
yashodip <yashodipp10@gmail.com>
2026-02-27 18:31:22 +0530

    using warpAsync instead of try and catch block for handling wrong input

--- App.js
+++ App.js
@@ -67,7 +67,7 @@ app.post("/listings", wrapAsync(async (req, res,next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-    });
+    }));
 
 //Edite route
 app.get("/listings/:id/edit",async(req,res)=>{
